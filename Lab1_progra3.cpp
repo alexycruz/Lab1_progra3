be6a861575555eac5c070cd2f4a304b43c7a43f0
alexycruz1 <xytojr@gmail.com>
2016-01-29 16:43:00 -0600

    Area_terminada

--- Lab1_progra3.cpp
+++ Lab1_progra3.cpp
@@ -12,6 +12,8 @@ double lados(double x1, double y1, double x2, double y2);
 double diagonal(double x1, double y2, double x4, double y4);
 double semiperimetro(double lado1, double lado2, double lado3);
 double perimetro(double lado1, double lado2, double lado3);
+double altura(double lado1, double lado2, double lado3, double semiperimetro);
+double Area_trapezoide(double diagonal, double altura_triangulo1, double altura_triangulo2);
 
 int main(int argc, char*argv[]){
 	int opcion;
@@ -19,6 +21,8 @@ int main(int argc, char*argv[]){
 	double x1, x2, x3, x4, y1, y2, y3, y4;
 	double lado1_1, lado1_2, lado1_3, lado2_1, lado2_2, lado2_3;
 	double semiperimetro1, semiperimetro2;
+	double diagonal_usar;
+	double altura_area;
 
 	cout << "Ingrese el programa al que quiere ingresar: ";
 	cout << endl;
@@ -75,7 +79,7 @@ int main(int argc, char*argv[]){
 		cout << "Lado 3: " << lados(x3, y3, x4, y4) << endl;
 		cout << "Lado 4: " << lados(x2, y2, x4, y4) << endl;
 		cout << endl;
-		//cout << diagonal(x1, y1, x4, y4);
+		diagonal_usar = diagonal(x1, y1, x4, y4);
 
 		cout << "Los lados del triangulo 1 miden: " << endl;
 		cout << "Lado 1: " << lados(x1, y1, x3, y3) << endl;
@@ -96,12 +100,28 @@ int main(int argc, char*argv[]){
 		cout << endl;
 
 		cout << "semiperimetro del triangulo 1: " << semiperimetro(lado1_1, lado1_2, lado1_3) << endl;
+		semiperimetro1 = semiperimetro(lado1_1, lado1_2, lado1_3);
+		semiperimetro2 = semiperimetro(lado2_1, lado2_2, lado2_3);
 		cout << "Semiperimetro del triangulo 2: " << semiperimetro(lado2_1, lado2_2, lado2_3) << endl;
 		cout << "Perimetro del triangulo 1: " << perimetro(lado1_1, lado1_2, lado1_3) << endl;
 		cout << "Perimetro del triangulo 2: " << perimetro(lado2_1, lado2_2, lado2_3) << endl;
 		cout << endl;
 
 		cout << "Las alturas del triangulo 1 miden: " << endl;
+		cout << "Altura 1: " << endl;
+		cout << "Altura 1: " << altura(lado1_1, lado1_2, lado1_3, semiperimetro1) << endl;
+		cout << "Altura 2: " << altura(lado1_2, lado1_1, lado1_3, semiperimetro1) << endl;
+		cout << "Altura 3: " << altura(lado1_3, lado1_1, lado1_2, semiperimetro1) << endl;
+		altura_area = altura(lado1_3, lado1_1, lado1_2, semiperimetro1);
+		cout << endl;
+
+		cout << "Las alturas del triangulo 2 miden: " << endl;
+		cout << "Altura 1: " << altura(lado2_1, lado2_2, lado2_3, semiperimetro2) << endl;
+		cout << "Altura 2: " << altura(lado2_2, lado2_1, lado2_3, semiperimetro2) << endl;
+		cout << "Altura 3: " << altura(lado2_3, lado2_1, lado2_2, semiperimetro2) << endl;
+		cout << endl;
+
+		cout << "El area del trapezoide es: " << Area_trapezoide(diagonal_usar, altura_area, altura_area) << endl;
 		}
 
 
@@ -150,4 +170,18 @@ double perimetro(double lado1, double lado2, double lado3){
 	double perimetro = (lado1 + lado2 + lado3);
 
 	return perimetro;
+}
+
+double altura(double lado1, double lado2, double lado3, double semiperimetro){
+	double resp;
+	resp = (2/lado1)*(sqrt(semiperimetro*((semiperimetro - lado1)*(semiperimetro - lado2)*(semiperimetro - lado3))));
+
+	return resp;
+}
+
+double Area_trapezoide(double diagonal, double altura_triangulo1, double altura_triangulo2){
+	double resp;
+	resp = ((diagonal * altura_triangulo1)/2 + (diagonal * altura_triangulo2)/2);
+
+	return resp;
 }
