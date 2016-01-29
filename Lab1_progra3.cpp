47f9ba2f63512ccc969dbeb5d89b1a2283e8732a
alexycruz1 <xytojr@gmail.com>
2016-01-29 16:10:22 -0600

    alturas

--- Lab1_progra3.cpp
+++ Lab1_progra3.cpp
@@ -6,12 +6,19 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int factorial(int x);
+unsigned long long int factorial(int x);
 double div(int x);
+double lados(double x1, double y1, double x2, double y2);
+double diagonal(double x1, double y2, double x4, double y4);
+double semiperimetro(double lado1, double lado2, double lado3);
+double perimetro(double lado1, double lado2, double lado3);
 
 int main(int argc, char*argv[]){
 	int opcion;
 	int limite_fact;
+	double x1, x2, x3, x4, y1, y2, y3, y4;
+	double lado1_1, lado1_2, lado1_3, lado2_1, lado2_2, lado2_3;
+	double semiperimetro1, semiperimetro2;
 
 	cout << "Ingrese el programa al que quiere ingresar: ";
 	cout << endl;
@@ -24,16 +31,88 @@ int main(int argc, char*argv[]){
 		cin >> limite_fact;
 
 		cout << div(limite_fact) + 1;
-	}else if (){
-	
+		cout << endl;
+	}else if (opcion == 2){
+		cout << "Ingrese x1: ";
+		cin >> x1;
+		cout << endl;
+		cout << "Ingrese y1: ";
+		cin >> y1;
+		cout << endl;
+		cout << "Ingrese x2: ";
+		cin >> x2;
+		cout << endl;
+		cout << "Ingrese y2: ";
+		cin >> y2;
+		cout << endl;
+		cout << "Ingrese x3: ";
+		cin >> x3;
+		cout << endl;
+		cout << "Ingrese y3: ";
+		cin >> y3;
+		cout << endl;
+		cout << "Ingrese x4: ";
+		cin >> x4;
+		cout << endl;
+		cout << "Ingrese y4: ";
+		cin >> y4;
+		cout << endl;
+
+		if (((x1 == x2) && (x1 == x3)) || ((y1 == y2) && (y1 == y3))){
+			cout << "Lo siento, tus puntos forman una recta, no podemos continuar" << endl;
+		}else if ((x1 == x2 && y1 == y2) || (x2 == x3 && y2 == y3) || (x3 == x1 && y3 == y1)){
+			cout << "Tienes dos puntos iguales, no podemos continuar" << endl;
+		}else{
+		cout << "(" << x1 << "," << y1 << ")" << endl;
+		cout << "(" << x2 << "," << y2 << ")" << endl;
+		cout << "(" << x3 << "," << y3 << ")" << endl;
+		cout << "(" << x3 << "," << y3 << ")" << endl;
+		cout << endl;
+
+		cout << "Los lados del trapezoide miden: " << endl;
+		cout << "Lado 1: " << lados(x1, y1, x2, y2) << endl;
+		cout << "Lado 2: " << lados(x1, y1, x3, y3) << endl;
+		cout << "Lado 3: " << lados(x3, y3, x4, y4) << endl;
+		cout << "Lado 4: " << lados(x2, y2, x4, y4) << endl;
+		cout << endl;
+		//cout << diagonal(x1, y1, x4, y4);
+
+		cout << "Los lados del triangulo 1 miden: " << endl;
+		cout << "Lado 1: " << lados(x1, y1, x3, y3) << endl;
+		lado1_1 = lados(x1, y1, x3, y3);
+		cout << "Lado 2: " << lados(x4, y4, x3, y3) << endl;
+		lado1_2 = lados(x4, y4, x3, y3);
+		cout << "Lado 3: " << lados(x1, y1, x4, y4) << endl;
+		lado1_3 = lados(x1, y1, x4, y4);
+		cout << endl;
+
+		cout << "Los lados del triangulo 2 miden: " << endl;
+		cout << "Lado 1: " << lados(x1, y1, x2, y2) << endl;
+		lado2_1 = lados(x1, y1, x2, y2);
+		cout << "Lado 2: " << lados(x2, y2, x4, y4) << endl;
+		lado2_2 = lados(x2, y2, x4, y4);
+		cout << "Lado 3: " << lados(x1, y1, x4, y4) << endl;
+		lado2_3 = lados(x1, y1, x4, y4);
+		cout << endl;
+
+		cout << "semiperimetro del triangulo 1: " << semiperimetro(lado1_1, lado1_2, lado1_3) << endl;
+		cout << "Semiperimetro del triangulo 2: " << semiperimetro(lado2_1, lado2_2, lado2_3) << endl;
+		cout << "Perimetro del triangulo 1: " << perimetro(lado1_1, lado1_2, lado1_3) << endl;
+		cout << "Perimetro del triangulo 2: " << perimetro(lado2_1, lado2_2, lado2_3) << endl;
+		cout << endl;
+
+		cout << "Las alturas del triangulo 1 miden: " << endl;
+		}
+
+
 	}
 
 	return 0;
 }//fin del main
 
 //metodos
-int factorial(int x){
-	int sum_acum = 1;
+unsigned long long int factorial(int x){
+	unsigned long long int sum_acum = 1;
 	for (int i = 1; i <= x; ++i){
 		sum_acum = sum_acum * i;
 	}
@@ -43,8 +122,32 @@ int factorial(int x){
 
 double div(int x){
 	double acum_total = 0;
-	for (int i = 1; i < 16; i++){
+	for (int i = 1; i < 22; i++){
 		acum_total += (pow(x, i))/(factorial(i));
 	}
 	return acum_total;
+}
+
+double lados(double x1, double y1, double x2, double y2){
+	double resp;
+	resp = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	return resp;
+}
+
+double diagonal(double x1, double y1, double x4, double y4){
+	double resp;
+	resp = sqrt(pow(x4 - x1, 2) + pow(y4 - y1, 2));
+	return resp;
+}
+
+double semiperimetro(double lado1, double lado2, double lado3){
+	double semiperimetro = (lado1 + lado2 + lado3)/2;
+
+	return semiperimetro;
+}
+
+double perimetro(double lado1, double lado2, double lado3){
+	double perimetro = (lado1 + lado2 + lado3);
+
+	return perimetro;
 }
