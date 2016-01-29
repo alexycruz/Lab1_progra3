2495ba5ce04efc534b43d7b6012defcb254802d4
alexycruz1 <xytojr@gmail.com>
2016-01-29 13:37:52 -0600

    Menu

---
+++ Lab1_progra3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int factorial(int x);
+
+int main(int argc, char*argv[]){
+	int opcion;
+
+cout << "Ingrese el programa al que quiere ingresar: ";
+cout << endl;
+cout << "1.- Ejercicio 1(Funcion de factorial)" << endl;
+cout << "2.- Ejercicio 2(Trapezoide)" << endl;
+cin >> opcion;
+
+if (opcion == 1){
+	
+}
+
+	return 0;
+}
